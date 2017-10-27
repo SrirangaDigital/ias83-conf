224a00f78c0ae3a452aa0f1b7b69140f1b823295
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-10-27 19:44:08 +0530

    map made responsive

--- application/views/flat/5-Information/Venue.php
+++ application/views/flat/5-Information/Venue.php
@@ -16,8 +16,10 @@
                 Umshing Mawkynroh, Shillong 793022<br />
                 Meghalaya, INDIA
             </h2>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.872054080345!2d91.89600324494793!3d25.609167478057213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37507f2687d8f6d1%3A0xc974c6a8be2e891d!2sMulti-Use+Convention+Hall!5e0!3m2!1sen!2sin!4v1508587735916" width="800" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-
+            <div class="responive-map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.872054080345!2d91.89600324494793!3d25.609167478057213!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37507f2687d8f6d1%3A0xc974c6a8be2e891d!2sMulti-Use+Convention+Hall!5e0!3m2!1sen!2sin!4v1508587735916" width="800" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+            
             <h2 id="bhopal_campus_map">Campus map</h2>
             <div id="viewimages">
                 <p><img src="<?=PUBLIC_URL?>images/stock/NEHU_campus_map.jpg" alt="NEHU Campus Map" title="Click to zoom" class="img-responsive"/></p>
