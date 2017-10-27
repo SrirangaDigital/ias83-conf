868646d7404e7d57e374c4e0a3aa234de66732ec
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-10-27 19:03:36 +0530

    twitter timeline added

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -285,7 +285,7 @@ $(document).ready(function() {
     </div>
     <div class="row" id="twitterContainer">
         <div class="col-md-6 col-sm-12">
-            <a class="twitter-timeline"  href="https://twitter.com/search?q=%23IASc83AM%20from%3A%40IAScBng" data-widget-id="874600019592003588">Tweets about #IASc83AM from:@IAScBng</a>
+            <a class="twitter-timeline"  href="https://twitter.com/search?q=%23IASc83AM%20from%3A%40IAScBng" data-widget-id="923905220559519744">Tweets about #IASc83AM from:@IAScBng</a>
             <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
         </div>
         <div class="col-md-6 text-center col-sm-12">
